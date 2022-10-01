f34668c42c957839abfbc991188acbfee67cbf7c
PuttTim <yannaputt@gmail.com>
2022-10-01 19:45:49 +0800

    Added maxWidth and Margins onto the App's Container area

--- portfix_web/src/App.tsx
+++ portfix_web/src/App.tsx
@@ -4,17 +4,20 @@ import Navbar from "./Components/Navbar"
 import Equipment from "./Pages/Equipment"
 import Dashboard from "./Pages/Dashboard"
 import Engineer from "./Pages/Engineer"
+import { Box } from "@chakra-ui/react"
 
 const App = () => {
     return (
         <>
             <Navbar />
-            <Routes>
-                <Route path="/" element={<Navigate to="/dashboard" />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/equipment" element={<Equipment />} />
-                <Route path="/engineer" element={<Engineer />} />
-            </Routes>
+            <Box maxWidth="1500" margin="auto" mb="8px">
+                <Routes>
+                    <Route path="/" element={<Navigate to="/dashboard" />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/equipment" element={<Equipment />} />
+                    <Route path="/engineer" element={<Engineer />} />
+                </Routes>
+            </Box>
         </>
     )
 }
