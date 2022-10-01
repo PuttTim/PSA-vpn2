f40ea9eb05e6fdc59cfae4a00f38e98f457d4d1a
Nasrullah <nasrullah01n@gmail.com>
2022-10-01 16:16:03 +0800

    Made ui a bit neater

--- portfix_mobile/lib/ui/screens/home/home_screen.dart
+++ portfix_mobile/lib/ui/screens/home/home_screen.dart
@@ -1,12 +1,8 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:portfix_mobile/data/tasks/task_model.dart';
 import 'package:portfix_mobile/data/tasks/task_repository.dart';
 import 'package:portfix_mobile/ui/screens/home/widgets/task_item.dart';
 import 'package:shimmer/shimmer.dart';
-import 'package:wave/config.dart';
-import 'package:wave/wave.dart';
 
 import 'package:portfix_mobile/ui/theme.dart';
 
@@ -24,54 +20,88 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("In Progress"),
+        title: const Text("Home"),
         elevation: 0,
         backgroundColor: CustomTheme.primary,
       ),
-      body: Stack(
-        children: [
-          wave(context),
-          StreamBuilder<List<TaskModel>>(
-            stream: _repository.getTaskStreams(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Shimmer.fromColors(
-                  period: const Duration(seconds: 1),
-                  child: Column(
-                    children: [
-                      loadingTile(context),
-                      loadingTile(context),
-                      loadingTile(context),
-                    ],
-                  ),
-                  baseColor: CustomTheme.getbaseColor(context).withAlpha(90),
-                  highlightColor:
-                      CustomTheme.gethighlightColor(context).withAlpha(120),
-                );
-              }
-              if (!snapshot.hasData || snapshot.data?.isEmpty == true) {
-                // show empty screen
-                return Container();
-              }
-
-              return Expanded(
-                child: ListView.separated(
-                  itemBuilder: (ctx, i) {
-                    return TaskItem(taskModel: snapshot.data![i]);
-                  },
-                  separatorBuilder: (ctx, i) {
-                    return Container(
-                      height: 1,
-                      color:
-                          Theme.of(context).colorScheme.onSurface.withAlpha(50),
-                    );
-                  },
-                  itemCount: snapshot.data!.length,
+      body: Container(
+        padding: const EdgeInsets.only(top: 20),
+        child: StreamBuilder<List<TaskModel>>(
+          stream: _repository.getTaskStreams(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Shimmer.fromColors(
+                period: const Duration(seconds: 1),
+                child: Column(
+                  children: [
+                    loadingTile(context),
+                    loadingTile(context),
+                    loadingTile(context),
+                  ],
                 ),
+                baseColor: CustomTheme.getbaseColor(context).withAlpha(90),
+                highlightColor:
+                    CustomTheme.gethighlightColor(context).withAlpha(120),
               );
-            },
-          )
-        ],
+            }
+            if (!snapshot.hasData || snapshot.data?.isEmpty == true) {
+              // show empty screen
+              return Container();
+            }
+            var inProgressList = snapshot.data!
+                .where((element) => element.engineerId?.isNotEmpty == true)
+                .toList();
+            inProgressList.sort((a, b) {
+              if (a.dueDate.millisecondsSinceEpoch >
+                  b.dueDate.millisecondsSinceEpoch) return 1;
+              return -1;
+            });
+
+            var others = snapshot.data!
+                .where((element) => element.engineerId?.isNotEmpty == false)
+                .toList();
+
+            others.sort((a, b) {
+              if (a.dueDate.millisecondsSinceEpoch >
+                  b.dueDate.millisecondsSinceEpoch) return 1;
+              return -1;
+            });
+
+            return Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.only(left: 16),
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "In Progress",
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                    ),
+                    Column(
+                      children: inProgressList
+                          .map((e) => TaskItem(taskModel: e))
+                          .toList(),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(left: 16),
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Others",
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                    ),
+                    Column(
+                      children:
+                          others.map((e) => TaskItem(taskModel: e)).toList(),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
@@ -88,27 +118,6 @@ class _HomeScreenState extends State<HomeScreen> {
       subtitle: Container(
         height: 15,
         color: CustomTheme.getbaseColor(context),
-      ),
-    );
-  }
-
-  Widget wave(BuildContext context) {
-    return Transform.rotate(
-      angle: pi,
-      child: WaveWidget(
-        config: CustomConfig(
-          durations: [4000],
-          heightPercentages: [0],
-          gradientBegin: Alignment.topCenter,
-          gradientEnd: Alignment.bottomCenter,
-          gradients: [
-            [CustomTheme.primary.withGreen(130), CustomTheme.primary]
-          ],
-        ),
-        size: Size(
-          double.infinity,
-          MediaQuery.of(context).size.height * 0.45,
-        ),
       ),
     );
   }
